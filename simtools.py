98ab08d9a343591719406b0da99ca37fa520bf7e
Shahir Kazi <shahirmuktasid22@gmail.com>
2026-01-31 21:34:52 +0300

    added a function to calculate the rsi between two trading dates

--- simtools.py
+++ simtools.py
@@ -4,9 +4,6 @@ from datetime import date, timedelta
 import holidays
 
 
-
-print(nyse_holidays)
-
 @jit
 def calculate_rsi(prev: np.float32, cur: np.float32, start_date: date, end_date: date):
     """
@@ -20,11 +17,21 @@ def calculate_rsi(prev: np.float32, cur: np.float32, start_date: date, end_date:
     :type duration: np.float32
     """
     end_date = end_date + timedelta(days=1)
-    delta = (end_date - start_date).days
+    delta = np.busday_count(start_date, end_date)
 
     # adjust for market holidays and weekends
+    nyse_days = holidays.financial_holidays("NYSE", years=range(start_date.year, end_date.year))
+
+    nyse_holidays: int = 0
+
+    for day in nyse_days:
+        if start_date <= day <= end_date:
+            nyse_days += 1
+
+    delta -= nyse_holidays
 
     return (cur-prev)/delta
+
 
 # @jit(nopython=True)
 # def simulate_price_by_duration(sec_price: np.float32, volume: np.int64, \
