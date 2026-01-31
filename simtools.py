5c3c61633eca159ca4db72b8717a2edaf8349bb0
Shahir Kazi <shahirmuktasid22@gmail.com>
2026-01-31 21:24:32 +0300

    change files a little

--- simtools.py
+++ simtools.py
@@ -1,11 +1,14 @@
 import numpy as np
 from numba import jit
-import datetime
+from datetime import date, timedelta
 import holidays
 
 
+
+print(nyse_holidays)
+
 @jit
-def calculate_rsi(prev: np.float32, cur: np.float32, start_date: , end_date: ):
+def calculate_rsi(prev: np.float32, cur: np.float32, start_date: date, end_date: date):
     """
     Docstring for calculate_rsi
     
@@ -16,13 +19,17 @@ def calculate_rsi(prev: np.float32, cur: np.float32, start_date: , end_date: ):
     :param duration: the duration days
     :type duration: np.float32
     """
-    return (cur-prev)/duration
+    end_date = end_date + timedelta(days=1)
+    delta = (end_date - start_date).days
 
+    # adjust for market holidays and weekends
 
-@jit(nopython=True)
-def simulate_price_by_duration(sec_price: np.float32, volume: np.int64, \
-                          open: np.float32, close: np.float32, high: np.float32, \
-                          low: np.float32, duration: np.int64,) -> np.ndarray:
-    # browninan bridge in numpy
+    return (cur-prev)/delta
 
-    return np.array([])
+# @jit(nopython=True)
+# def simulate_price_by_duration(sec_price: np.float32, volume: np.int64, \
+#                           open: np.float32, close: np.float32, high: np.float32, \
+#                           low: np.float32, duration: np.int64,) -> np.ndarray:
+#     # browninan bridge in numpy
+
+#     return np.array([])
