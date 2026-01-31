70ede199ff50fd66e7896ea8d9e59b753c8a7315
Shahir Kazi <shahirmuktasid22@gmail.com>
2026-01-31 23:27:14 +0300

    small updates

--- simtools.py
+++ simtools.py
@@ -5,24 +5,26 @@ import holidays
 
 
 @jit
-def calculate_rsi(prev: np.float32, cur: np.float32, start_date: date, end_date: date) -> np.float64:
+def calculate_rsi(prev: np.float32, cur: np.float32, start_date: date, end_date: date) -> np.float32:
     """
     Docstring for calculate_rsi
     
     :param prev: the price of the security in the backwards date
     :type prev: np.float32
     :param cur: the present price
-    :type cur: datetime.date 
-    :param duration: the duration days
-    :type duration: datetime.date
+    :type cur: np.float32
+    :param start_date: the backwards date
+    :type start_date: datetime.date
+    :param end_date: the forwards date
+    :type end_date: datetime.date
     """
     end_date = end_date + timedelta(days=1)
-    delta = np.busday_count(start_date, end_date)
+    delta = np.float32(np.busday_count(start_date, end_date))
 
     # adjust for market holidays and weekends
     nyse_days = holidays.financial_holidays("NYSE", years=range(start_date.year, end_date.year))
 
-    nyse_holidays = np.float64(0)
+    nyse_holidays = np.float32(0)
 
     for day in nyse_days:
         if start_date <= day <= end_date:
@@ -30,7 +32,9 @@ def calculate_rsi(prev: np.float32, cur: np.float32, start_date: date, end_date:
 
     delta -= nyse_holidays
 
-    return (end_date-start_date).days/delta
+    return (cur-prev)/delta
+
+
 
 
 # @jit(nopython=True)
