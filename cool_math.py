eb8abb701d1165018a7f48afe1f94323354e18fa
Shahir Kazi <shahirmuktasid22@gmail.com>
2026-02-06 00:58:42 +0300

    minor fix

--- cool_math.py
+++ cool_math.py
@@ -1,6 +1,7 @@
 import numpy as np
 from datetime import date, timedelta
 from enum import Enum
+from scipy.special import comb
 
 
 # I created this to learn the concepts in statistics better
@@ -17,10 +18,10 @@ def simple_return(cur_price: np.float32, old_price: np.float32) -> np.float32:
 
     return (cur_price-old_price)/old_price
 
-def brownian_bridge(number_of_items: np.int32, kurtosis: np.float32, bias: np.float32):
+def brownian_bridge(number_of_items: np.int32, kurtosis: np.float32, bias: np.float32) -> np.ndarray:
     ## implement
-    return
+    return np.array([])
 
 
-def create_fake_stock_data(ticker: str, duration: timedelta, start: date, end: date, granularity: Granularity):
-    return
+def create_fake_stock_data(ticker: str, duration: timedelta, start: date, end: date, granularity: Granularity) -> np.ndarray:
+    return np.array([])
